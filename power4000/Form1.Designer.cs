18af30344311332c5ee16a945179e7716ee51d55
Rozin <zonjamdda@gmail.com>
2024-07-07 02:20:07 +0900

    초기 커밋.

---
+++ power4000/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace power4000
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle21 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle22 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle23 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle24 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle25 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_end = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtip = new System.Windows.Forms.TextBox();
+            this.txtport = new System.Windows.Forms.TextBox();
+            this.Dgv = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.col_Seq = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_Mid = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_Msg = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(351, 82);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 0;
+            this.btn_start.Text = "start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_end
+            // 
+            this.btn_end.Location = new System.Drawing.Point(443, 82);
+            this.btn_end.Name = "btn_end";
+            this.btn_end.Size = new System.Drawing.Size(75, 23);
+            this.btn_end.TabIndex = 1;
+            this.btn_end.Text = "End";
+            this.btn_end.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port";
+            // 
+            // txtip
+            // 
+            this.txtip.Location = new System.Drawing.Point(34, 83);
+            this.txtip.Name = "txtip";
+            this.txtip.Size = new System.Drawing.Size(100, 21);
+            this.txtip.TabIndex = 4;
+            // 
+            // txtport
+            // 
+            this.txtport.Location = new System.Drawing.Point(201, 83);
+            this.txtport.Name = "txtport";
+            this.txtport.Size = new System.Drawing.Size(100, 21);
+            this.txtport.TabIndex = 5;
+            // 
+            // Dgv
+            // 
+            this.Dgv.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.Dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.col_Seq,
+            this.col_Time,
+            this.col_Type,
+            this.col_Mid,
+            this.col_Msg});
+            this.Dgv.GridColor = System.Drawing.SystemColors.Control;
+            this.Dgv.Location = new System.Drawing.Point(12, 317);
+            this.Dgv.Name = "Dgv";
+            this.Dgv.ReadOnly = true;
+            this.Dgv.RowTemplate.Height = 23;
+            this.Dgv.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Dgv.Size = new System.Drawing.Size(826, 221);
+            this.Dgv.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSlateGray;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(4, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(842, 36);
+            this.panel1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Networking Setting";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 302);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Data Log";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(1, -6);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(601, 37);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Tighten PowerFocus / PowerMac Server(v1.0)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(809, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(773, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 30);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // col_Seq
+            // 
+            dataGridViewCellStyle21.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle21.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.col_Seq.DefaultCellStyle = dataGridViewCellStyle21;
+            this.col_Seq.HeaderText = "SEQ";
+            this.col_Seq.Name = "col_Seq";
+            this.col_Seq.ReadOnly = true;
+            // 
+            // col_Time
+            // 
+            dataGridViewCellStyle22.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle22.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.col_Time.DefaultCellStyle = dataGridViewCellStyle22;
+            this.col_Time.HeaderText = "TIME";
+            this.col_Time.Name = "col_Time";
+            this.col_Time.ReadOnly = true;
+            // 
+            // col_Type
+            // 
+            dataGridViewCellStyle23.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle23.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.col_Type.DefaultCellStyle = dataGridViewCellStyle23;
+            this.col_Type.HeaderText = "TYPE";
+            this.col_Type.Name = "col_Type";
+            this.col_Type.ReadOnly = true;
+            // 
+            // col_Mid
+            // 
+            dataGridViewCellStyle24.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle24.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.col_Mid.DefaultCellStyle = dataGridViewCellStyle24;
+            this.col_Mid.HeaderText = "MID";
+            this.col_Mid.Name = "col_Mid";
+            this.col_Mid.ReadOnly = true;
+            // 
+            // col_Msg
+            // 
+            this.col_Msg.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle25.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle25.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.col_Msg.DefaultCellStyle = dataGridViewCellStyle25;
+            this.col_Msg.HeaderText = "Message";
+            this.col_Msg.Name = "col_Msg";
+            this.col_Msg.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 550);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn_end);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.txtport);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Dgv);
+            this.Controls.Add(this.txtip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_end;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtip;
+        private System.Windows.Forms.TextBox txtport;
+        private System.Windows.Forms.DataGridView Dgv;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_Seq;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_Time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_Type;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_Mid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_Msg;
+    }
+}
+
---
+++ power4000/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace power4000
+{
+    public partial class Form1 : Form
+    {
+        private Thread keepAliveThread;
+        private Socket socket;
+        private int sequenceNumber = 0; // Sequence number for col_Seq
+        private bool stopKeepAlive = false; // Flag to stop the keep-alive thread
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtip.Text = GetLocalIPAddress();
+            txtport.Text = "4545";
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            string serverIp = txtip.Text;
+            int serverPort = int.Parse(txtport.Text);
+
+            try
+            {
+                // Create a TCP/IP socket.
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPAddress ipAddress = IPAddress.Parse(serverIp);
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, serverPort);
+
+                // Connect to the remote endpoint.
+                socket.Connect(remoteEP);
+
+                // Execute message sequences
+                if (SendInitialMessage(socket))
+                {
+                    if (SendSecondMessage(socket))
+                    {
+                        if (SendThirdMessage(socket))
+                        {
+                            if (SendFourthMessage(socket))
+                            {
+                                // Start keep-alive thread after the last message
+                                StartKeepAliveThread();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message);
+            }
+        }
+
+        private bool SendInitialMessage(Socket socket)
+        {
+            return SendAndReceive(socket, "00200001001         ", "0002");
+        }
+
+        private bool SendSecondMessage(Socket socket)
+        {
+            return SendAndReceive(socket, "00200060001         ", "0005");
+        }
+
+        private bool SendThirdMessage(Socket socket)
+        {
+            return SendAndReceive(socket, "00200051001         ", "0005");
+        }
+
+        private bool SendFourthMessage(Socket socket)
+        {
+            return SendAndReceive(socket, "00200034001         ", "0005");
+        }
+
+        private bool SendAndReceive(Socket socket, string message, string expectedResponse)
+        {
+            // Send data to the server
+            SendData(socket, message);
+
+            // Receive data from the server
+            string responseData = ReceiveData(socket);
+
+            // Extract col_Mid value from received data
+            string colMidValue = responseData.Substring(4, 4);
+
+            // Display received data in Dgv
+            DisplayDataInDgv(responseData, "RECV");
+
+            // Check if the response matches the expected value
+            if (colMidValue == expectedResponse)
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show($"Unexpected response received: {colMidValue}");
+                return false;
+            }
+        }
+
+        private void SendData(Socket socket, string message)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(message);
+            socket.Send(data);
+
+            // Display sent data in Dgv
+            DisplayDataInDgv(message, "SEND");
+        }
+
+        private string ReceiveData(Socket socket)
+        {
+            byte[] buffer = new byte[256];
+            int bytesRead = socket.Receive(buffer);
+            string responseData = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+            return responseData;
+        }
+
+        private void DisplayDataInDgv(string data, string type)
+        {
+            string colMidValue = data.Substring(4, 4);
+            string currentTime = DateTime.Now.ToString("HH:mm:ss");
+            int rowIndex = (int)Dgv.Invoke((Func<int>)(() => Dgv.Rows.Add()));
+
+            Dgv.Invoke((Action)(() =>
+            {
+                Dgv.Rows[rowIndex].Cells["col_Seq"].Value = sequenceNumber++;
+                Dgv.Rows[rowIndex].Cells["col_Time"].Value = currentTime;
+                Dgv.Rows[rowIndex].Cells["col_Mid"].Value = colMidValue;
+                Dgv.Rows[rowIndex].Cells["col_Type"].Value = type;
+            }));
+
+            // Check if received data is "0052"
+            if (colMidValue == "0052")
+            {
+                // Stop keep-alive thread
+                stopKeepAlive = true;
+                // Wait for 2 seconds before sending the response
+                Thread.Sleep(2000);
+                // Send the response message
+                SendResponseMessage();
+                // Restart keep-alive thread
+                StartKeepAliveThread();
+            }
+            // Check if received data is "0035"
+            else if (colMidValue == "0035")
+            {
+                // Stop keep-alive thread
+                stopKeepAlive = true;
+                // Send the response message for "0035"
+                SendData(socket, "00200036001         ");
+                // Restart keep-alive thread
+                StartKeepAliveThread();
+            }
+            else if (colMidValue == "0061")
+            {
+                // Stop keep-alive thread
+                stopKeepAlive = true;
+                // Send the response message for "0035"
+                SendData(socket, "00200062001         ");
+                // Restart keep-alive thread
+                StartKeepAliveThread();
+            }
+        }
+
+        private void StartKeepAliveThread()
+        {
+            stopKeepAlive = false;
+            keepAliveThread = new Thread(KeepConnectionAlive);
+            keepAliveThread.IsBackground = true; // Make sure the thread ends when the main application ends
+            keepAliveThread.Start();
+        }
+
+        private void KeepConnectionAlive()
+        {
+            while (!stopKeepAlive)
+            {
+                try
+                {
+                    if (socket != null && socket.Connected)
+                    {
+                        // Send keep-alive message
+                        string keepAliveMessage = "00209999001         ";
+                        SendData(socket, keepAliveMessage);
+
+                        // Receive response from server
+                        string responseData = ReceiveData(socket);
+
+                        // Display received data in Dgv
+                        DisplayDataInDgv(responseData, "RECV");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Exception in keep-alive: " + ex.Message);
+                    break;
+                }
+                Thread.Sleep(3000); // 3 seconds interval
+            }
+        }
+
+        private void SendResponseMessage()
+        {
+            string responseMessage = "00200053001         ";
+            SendData(socket, responseMessage);
+        }
+
+        private string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("Local IP Address Not Found!");
+        }
+    }
+}
